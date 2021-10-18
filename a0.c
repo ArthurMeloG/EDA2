78b0cb8607dee21542f4fdc44815a931da2d0256
Eliás Yousef <eliasyousef00@gmail.com>
2021-10-18 19:20:43 -0300

    .

--- a0.c
+++ a0.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define true 1
 #define false 0
+
 
 typedef struct Posicao {
     int linha;
     int coluna;
 }Posicao;
 
-int verifica (Posicao posicaoAtual, int entradaLinha, int entradaColuna) {
-    if(entradaLinha == posicaoAtual.linha && entradaColuna == posicaoAtual.coluna )
+
+int verifica (Posicao posicaoAtual, int entradaLinha, int entradaColuna ) {
+    if ( entradaLinha == posicaoAtual.linha && entradaColuna == posicaoAtual.coluna )
         return true;
 
     return false; 
 }
 
+
+void fazSonda (long int L, long int C, long int EDAzinhos) {
+
+}
+
+
 int main () {
 
-    long int L, C, P, T, EDAzinhos;
+    long int L, C, P, T, EDAzinhos = 1, pontos;
+    char opcao[100];
 
-    scanf(" %ld %ld %ld %ld", &L, &C, &P, &T);
-    
+    scanf ( "%ld %ld %ld %ld", &L, &C, &P, &T );
+    pontos = P;
+
     for (int i = 0; i < T; i++) {
 
+        scanf ( "%s", opcao );
+        scanf ( "%ld %ld", &L, &C );
+
+        if ( strcmp ( opcao, "sondar") == false ) {
+            fazSonda ( L, C, EDAzinhos );
+        }
+
+        if ( strcmp ( opcao, "dominar" ) == false ) {
+            break;
+        }
+
+        else {
+
+        }
     }
 
     return 0;
