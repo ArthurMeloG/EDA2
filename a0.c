a7031507b90c8a6a26aa6ed31653577bed6037c5
Eliás Yousef <eliasyousef00@gmail.com>
2021-10-18 18:24:15 -0300

    includes

---
+++ a0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    long int L, C, P, T;
+
+    scanf(" %ld %ld %ld %ld", &L, &C, &P, &T);
+    printf("%ld %ld %ld %ld\n", L, C, P, T);
+
+
+    return 0;
+}
