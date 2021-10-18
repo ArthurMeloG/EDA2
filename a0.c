a167abd0f6c1ebee4bf3e42f83503bd442f4477e
Eliás Yousef <eliasyousef00@gmail.com>
2021-10-18 18:44:50 -0300

    .

--- a0.c
+++ a0.c
@@ -3,12 +3,12 @@
 
 int main () {
 
-    long int L, C, P, T;
+    long int L, C, P, T, EDAzinhos;
 
     scanf(" %ld %ld %ld %ld", &L, &C, &P, &T);
     
     for (int i = 0; i < T; i++) {
-        
+
     }
 
     return 0;
