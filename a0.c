eabd28c5b0679d15d163865ec12c208f3d7e4806
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-10-18 18:41:02 -0300

    Struct

--- a0.c
+++ a0.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define true 1
+#define false 0
+
+typedef struct Posicao {
+    int linha;
+    int coluna;
+}Posicao;
+
+int verifica (Posicao posicaoAtual, int entradaLinha, int entradaColuna) {
+    if(entradaLinha == posicaoAtual.linha && entradaColuna == posicaoAtual.coluna )
+        return true;
+
+    return false; 
+}
+
 int main () {
 
     long int L, C, P, T;
